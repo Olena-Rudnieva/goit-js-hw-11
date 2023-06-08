034b3a70e93fe29141ce37bcc7fd15fe4918c66e
Olena-Rudnieva <rudnievao@gmail.com>
2023-06-08 21:05:18 +0300

    Update js

--- src/index.js
+++ src/index.js
@@ -8,31 +8,67 @@ import { Loading } from 'notiflix/build/notiflix-loading-aio';
 import { Block } from 'notiflix/build/notiflix-block-aio';
 
 const formEl = document.querySelector('.search-form');
+const galleryEl = document.querySelector('.gallery');
+galleryEl.style.display = 'flex';
+galleryEl.style.flexWrap = 'wrap';
+galleryEl.style.marginTop = '30px';
+galleryEl.style.gap = '20px';
+
 formEl.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(evt) {
     evt.preventDefault()
-    console.log(formEl.elements.searchQuery.value);    
+    const query = formEl.elements.searchQuery.value;  
+    fetchPhoto(query)
+      .then(option => {  
+          option.hits.map(value => renderPhoto(value))
+               
+      })
+      .catch(error => {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );      
+        console.log(error);
+      });
 }
 
-// https://pixabay.com/api/?key=37130379-4004eb1f0f9bfd5f433c52abe&q=yellow+flowers&image_type=photo&orientation=horizontal&safesearch=true
+// axios.get('https://pixabay.com/api/?key=37130379-4004eb1f0f9bfd5f433c52abe&q=yellow+flowers&image_type=photo&orientation=horizontal&safesearch=true').then(data => console.log(data))
+
+function fetchPhoto(q) {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const API_KEY = '37130379-4004eb1f0f9bfd5f433c52abe';
+
+  return fetch(
+    `${BASE_URL}?key=${API_KEY}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    return response.json();
+  });
+}
 
 
-
-{/* <div class="photo-card">
-  <img src="" alt="" loading="lazy" />
+function renderPhoto(obj) {
+  const { previewURL, tags, likes, views, comments, downloads } = obj;
+  const markup = `
+    <div class="photo-card">
+  <img src="${previewURL}" alt="${tags}" loading="lazy" />
   <div class="info">
     <p class="info-item">
-      <b>Likes</b>
+      <b>Likes ${likes}</b>
     </p>
     <p class="info-item">
-      <b>Views</b>
+      <b>Views ${views}</b>
     </p>
     <p class="info-item">
-      <b>Comments</b>
+      <b>Comments ${comments}</b>
     </p>
     <p class="info-item">
-      <b>Downloads</b>
+      <b>Downloads ${downloads}</b>
     </p>
   </div>
-</div>; */}
+</div>`;
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+}
